b707f97e16c0902336ad0005a7f5533953a5ae17
Abhijit Radhakrishnan <peringadanabhijit@gmail.com>
2025-12-21 09:02:14 +0530

    Configure Tailwind and update root layout

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.css"; // tailwind
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { ThemeProvider } from "@/context/Theme/ThemeContext";
 
 export const metadata = {
   title: "Portfolio - Abhijit Perindadan",
@@ -21,12 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar/>
-          {children}
-        <Footer/>
+      <body className={`antialiased`}>
+        <ThemeProvider>
+          <Navbar/>
+            {children}
+          <Footer/>
+        </ThemeProvider>
       </body>
     </html>
   );
